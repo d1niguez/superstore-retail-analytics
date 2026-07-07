53b6ed06c4886bb9c9a409064bf0cf93ecea7ae7
Danna <iniguezdanna0523@gmail.com>
2026-07-07 14:06:41 -0500

    Add category and sub-category profitability and discount queries

--- queries.sql
+++ queries.sql
@@ -1,10 +1,10 @@
 
 -- Query 1: Regional Profitability
 SELECT
-Region,
-ROUND(SUM(Sales), 2) AS total_sales,
-ROUND(SUM(Profit), 2) AS total_profit,
-ROUND(SUM(Profit) / SUM(Sales) *100, 2) AS profit_margin_pct
+	Region,
+	ROUND(SUM(Sales), 2) AS total_sales,
+	ROUND(SUM(Profit), 2) AS total_profit,
+	ROUND(SUM(Profit) / SUM(Sales) *100, 2) AS profit_margin_pct
 FROM orders
 GROUP BY Region
 ORDER BY total_profit DESC;
@@ -12,9 +12,31 @@ ORDER BY total_profit DESC;
 
 -- Query 2: Discount Impact by Region
 SELECT
-Region,
-ROUND(AVG(Discount), 3) AS avg_discount,
-ROUND(SUM(Profit) / SUM(Sales) * 100, 2) AS profit_margin_pct
+	Region,
+	ROUND(AVG(Discount), 3) AS avg_discount,
+	ROUND(SUM(Profit) / SUM(Sales) * 100, 2) AS profit_margin_pct
 FROM orders
 GROUP BY Region
 ORDER BY avg_discount DESC;
+
+-- Query 3: Category and Subcategory Performance
+SELECT
+	Category,
+	`Sub-Category`,
+	ROUND(SUM(Sales), 2) AS total_sales,
+	ROUND(SUM(Profit) , 2) AS total_profit,
+	ROUND(SUM(Profit) / SUM(Sales)* 100, 2) AS profit_margin_pct
+FROM orders
+GROUP BY  Category, `Sub-Category`
+ORDER BY total_profit ASC;
+
+-- Query 4: Discount impact by Sub-Category
+SELECT 
+	Category,
+    `Sub-Category`,
+    ROUND(AVG(Discount), 3) AS avg_discount,
+    ROUND(SUM(Profit) / SUM(Sales) * 100, 2) AS profit_margin_pct
+FROM orders
+GROUP BY Category, `Sub-Category`
+ORDER BY avg_discount DESC;
+    
