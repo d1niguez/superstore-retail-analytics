0b34d9ce27d540f37a5bb68c149d7b9470f3fc6e
Danna <iniguezdanna0523@gmail.com>
2026-07-09 12:26:52 -0500

    Cleaned up queries making them consistent throughout the file.

--- queries.sql
+++ queries.sql
@@ -45,7 +45,7 @@ SELECT
 	Segment,
     ROUND(SUM(Sales), 2) AS total_sales,
     ROUND(SUM(Profit), 2) AS total_profit,
-    ROUND(SUM(Profit) / SUM(Sales) * 100, 2) AS profit_margin_pct,
+	ROUND(SUM(Profit) / NULLIF(SUM(Sales), 0) * 100, 2) AS profit_margin_pct,
     COUNT(DISTINCT `Order ID`) AS total_orders
 FROM orders
 GROUP BY Segment
@@ -53,12 +53,12 @@ ORDER BY total_profit DESC;
     
 -- Query 6: Monthly Sales and Profit Trend
 SELECT
-	YEAR (order_date_fixed) AS order_year,
-    MONTH (order_date_fixed) AS order_month,
+	YEAR(order_date_fixed) AS order_year,
+    MONTH(order_date_fixed) AS order_month,
     ROUND(SUM(Sales), 2) AS total_sales,
     ROUND(SUM(Profit), 2) AS total_profit
-FROM Orders
-GROUP BY YEAR(order_date_fixed),MONTH(order_date_fixed)
+FROM orders
+GROUP BY YEAR(order_date_fixed), MONTH(order_date_fixed)
 ORDER BY order_year, order_month;
 
 -- Query 7: Central Region  - Category Breakdown
@@ -67,7 +67,7 @@ SELECT
     `Sub-Category`,
     ROUND(SUM(Sales), 2) AS total_sales,
     ROUND(SUM(Profit), 2) AS total_profit,
-    ROUND(SUM(Profit) / SUM(Sales) * 100, 2) AS prrofit_margin_pct,
+	ROUND(SUM(Profit) / NULLIF(SUM(Sales), 0) * 100, 2) AS profit_margin_pct,
     ROUND(AVG(Discount), 3) AS avg_discount
 FROM orders
 WHERE Region = 'Central'
